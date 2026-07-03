bf7e5e20c6eee79e8fa110a047aecf8632a85a6b
Mateusz Wasielewski <dsw60378@student.dsw.edu.pl>
2026-07-03 10:19:09 +0200

    Task1 - parsowanie argumentów

--- project.py
+++ project.py
@@ -1,1 +1,20 @@
-print("Program działa!")
+import sys
+import os
+
+# Sprawdzenie liczby argumentów
+if len(sys.argv) != 3:
+    print("Użycie programu:")
+    print("python project.py plik_wejściowy plik_wyjściowy")
+    exit()
+
+# Pobranie nazw plików
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# Sprawdzenie czy plik wejściowy istnieje
+if not os.path.exists(input_file):
+    print("Błąd! Plik wejściowy nie istnieje.")
+    exit()
+
+print("Plik wejściowy:", input_file)
+print("Plik wyjściowy:", output_file)
